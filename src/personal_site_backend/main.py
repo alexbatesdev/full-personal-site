9fc6ed724f995d4b91020c5fa6a632c1c1281d27
Alex Bates <alexbates.dev@gmail.com>
2026-09-24 20:19:21 -0600

    Serve personal site but in a local only way. super dev commit go!!!!!!

--- src/personal_site_backend/main.py
+++ src/personal_site_backend/main.py
@@ -1,15 +1,26 @@
+from pathlib import Path
+
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 import uvicorn
 
+
 from personal_site_backend.api.router import api_router
+
+WEBSITE_DIRECTORY = Path(
+    r"C:\Users\Alex\Documents\My_Documents__\Personal Code\neocity-website"
+)
 
 app = FastAPI(title="Personal Site Backend")
 
 app.include_router(api_router)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+app.mount(
+    "/",
+    StaticFiles(directory=WEBSITE_DIRECTORY, html=True),
+    name="website",
+)
+
 
 def main() -> None:
-	uvicorn.run("personal_site_backend.main:app", reload=True)
+    uvicorn.run("personal_site_backend.main:app", reload=True)
